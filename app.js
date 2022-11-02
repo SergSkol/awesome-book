5371316c830cfaf664f23d940dfca8ca0d14dca7
Serg Skol <sps676@gmail.com>
2022-11-02 09:24:02 +0100

    Change remove to filter

--- app.js
+++ app.js
@@ -3,33 +3,24 @@ const currentFormTitle = document.querySelector('#title');
 const currentFormAuthor = document.querySelector('#author');
 const currentFormBookAddButton = document.querySelector('#book-add-button');
 
-class Book {
-  constructor(title, author) {
-    let newId = 0;
-    const timeNow = new Date();
-    newId = timeNow.getTime();
-
-    this.id = newId;
-    this.title = title;
-    this.author = author;
-  }
-}
-
 class BookShelf {
   constructor() {
     this.arrBooks = [];
   }
 
-  addBook(newBook) {
+  addBook(newTitle, newAuthor) {
+    const timeNow = new Date();
+    const newBook = {
+      id: timeNow.getTime(),
+      title: newTitle,
+      author: newAuthor
+    };
+
     this.arrBooks.push(newBook);
   }
 
   removeBook(id) {
-    for (let i = 0; i < this.arrBooks.length; i += 1) {
-      if (this.arrBooks[i].id === id) {
-        this.arrBooks.splice(i, 1);
-      }
-    }
+    this.arrBooks = this.arrBooks.filter((book) => book.id !== id);
   }
 
   saveDataToLocalStorage() {
@@ -62,22 +53,7 @@ class BookShelf {
   }
 
   loadDataFromLocalStorage() {
-    const dataLoaded = JSON.parse(localStorage.getItem(storageKey));
-
-    if (dataLoaded !== null) {
-      dataLoaded.forEach((book) => {
-        const newBook = {
-          id: 0,
-          title: '',
-          author: '',
-        };
-
-        newBook.id = book.id;
-        newBook.title = book.title;
-        newBook.author = book.author;
-        this.arrBooks.push(newBook);
-      });
-    }
+    this.arrBooks = JSON.parse(localStorage.getItem(storageKey));
     this.showBooks();
   }
 }
@@ -87,8 +63,7 @@ const awesomeBookShelf = new BookShelf();
 currentFormBookAddButton.addEventListener('click', () => {
   const title = currentFormTitle.value;
   const author = currentFormAuthor.value;
-  const newBook = new Book(title, author);
-  awesomeBookShelf.addBook(newBook);
+  awesomeBookShelf.addBook(title, author);
   awesomeBookShelf.saveDataToLocalStorage();
 });
 
